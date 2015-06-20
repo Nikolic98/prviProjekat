f8688f4d2fef85288630ce55d07a669dfb484fde
Nikolic98 <markonikolic770@gmail.com>
2015-06-20 16:37:34 +0200

    novo

---
+++ connect_to_mysql.php.php
@@ -0,0 +1,12 @@
+
+<?php
+
+$db_host="localhost";
+$db_username="root";
+$db_pass="123";
+$db_name="prviprojekat";
+
+mysql_connect("$db_host","$db_username","$db_pass") or die(mysql_error());
+mysql_select_db("$db_name")or die("no database by that name");
+?>
+
