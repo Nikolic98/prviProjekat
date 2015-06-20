74708d045056bfaf75a5fbb50d8247d1fbc097be
Nikolic98 <markonikolic770@gmail.com>
2015-06-20 15:59:39 +0200

    Ovo je pocetak

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$db_host="localhost";
+$db_username="put_db_username_here";
+$db_pass="put_db_password_her";
+$db_name="put_db_name_her";
+
+mysql_connect("$db_host","$db_username","$db_pass") or die(mysql_error());
+?>
+
