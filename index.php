96a383a00608c305b111802a52551f05f3c71376
Nikolic98 <markonikolic770@gmail.com>
2015-06-22 12:00:48 +0200

    Sajt

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+	<head>
+			<title>Register</title>
+	</head>
+	<body>
+	</body>
+</html>
--- mysql_connect_quicktest.php
+++ mysql_connect_quicktest.php
@@ -1,6 +1,6 @@
 <?php
 
 include_once"connect_to_mysql.php";
-print"<h2>Bravo</h2>";
+print"<h2>Bravo, uspeo si</h2>";
 
 ?>
